eed7a36557f737650bca7801183263442570583c
Cookie <61884850+CookieHax@users.noreply.github.com>
2021-12-21 12:43:44 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from boto.s3.connection import S3Connection
 from fastapi import FastAPI
 import os
 import requests
-Discord_Webhook = S3Connection(os.environ['Discord_Webhook'])
-Website_Api = S3Connection(os.environ['Website_Key'])
+Discord_Webhook = S3Connection(os.environ['Auth_key'], )
+Website_Api = S3Connection(os.environ['Website_Key'], os.environ['Auth_key'])
 
 def SendMessageToDiscord(Msg):
     Message = {
