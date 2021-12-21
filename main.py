77a00b0551ff27444b5f71291c8dbe291a29a03f
Cookie <61884850+CookieHax@users.noreply.github.com>
2021-12-21 11:30:29 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+from boto.s3.connection import S3Connection
+from fastapi import FastAPI
+import os
+import requests
+Discord_Webhook = S3Connection(os.environ['Discord_Webhook'])
+Website_Api = S3Connection(os.environ['Website_Key'])
+
+def SendMessageToDiscord(Msg):
+    Message = {
+        "content": Msg
+    }
+    requests.post(Discord_Webhook, data=Message)
+
+# app = FastAPI()
+
+@app.get("/discord")
+async def read_items(Key: str, Message: str):
+    if Key == Website_Api:
+      SendMessageToDiscord(Message)
+    else:
+        return "Incorrect key"
